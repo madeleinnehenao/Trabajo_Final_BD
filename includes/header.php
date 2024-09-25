a04a43477ef69f3cdd9e0e20404899020c2df76a
Goezduque92 <hgoez@a-maq.com>
2024-09-24 20:22:11 -0500

    Inicio del proyecto

---
+++ includes/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trabajo final BD1</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <span class="fs-4">Biblioteca Barrancabermeja</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">Inicio</a></li>
+        <li class="nav-item"><a href="/consultas/consulta1" class="nav-link">Consulta 1</a></li>
+        <li class="nav-item"><a href="/consultas/consulta2" class="nav-link">Consulta 2</a></li>
+        <li class="nav-item"><a href="/busqueda/busqueda1" class="nav-link">Búsqueda 1</a></li>
+        <li class="nav-item"><a href="/busqueda/busqueda2" class="nav-link">Búsqueda 2</a></li>
+      </ul>
+    </header>
+  </div>
+
+
+        
